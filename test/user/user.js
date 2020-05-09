88ac7ab20a4cf8940f09ab1222a1b087f18b8a6f
zw <zzw1366@163.com>
2020-05-09 18:45:09 +0800

    user 单元测试

---
+++ test/user/user.js
@@ -0,0 +1,13 @@
+// 用户数据
+const testUser = {
+  id: '99099',
+  // userName: '13800138123',
+  password: 'pwdpwd',
+  nickName: '测试1',
+  gender: 1,
+  provinceId: '130000',
+  cityId: '130300',
+  headImg: 'http://106.13.204.211/headList/10.jpg'
+}
+
+module.exports = testUser
--- test/user/user.test.js
+++ test/user/user.test.js
@@ -3,17 +3,20 @@
  * @author zzw
  */
 const server = require('../server');
+const testUser = require('./user')
 
 const oneUser = {
   userName: `${Date.now()}`.slice(0, 11),
   password: 'psd123456',
   nickName: 'nickName',
   gender: 2,
-  headImg: 'http://106.13.204.211/headList/6.jpg'
+  headImg: 'http://106.13.204.211/headList/6.jpg',
+  provinceId: '130000',
+  cityId: '130300',
+
 }
 let cookie = ''
 
-console.log('oneUser：', oneUser)
 
 /**
  * 注册
@@ -75,9 +78,41 @@ test('登录失败 密码不一致', async () => {
 })
 
 /**
+ * 修改用户信息
+ */
+test('修改用户信息', async () => {
+  const res = await server
+    .post('/api/user/update')
+    .set('cookie', cookie)
+    .send(testUser)
+  expect(res.body.code).toBe(0)
+})
+
+/**
+ * 查询用户信息
+ */
+test('查询用户信息', async () => {
+  const result = {
+    ...testUser,
+    gender: testUser.gender.toString(),
+    userName: oneUser.userName
+  }
+  delete result.password
+  const res = await server
+    .post('/api/user/getUserInfo')
+    .set('cookie', cookie)
+    .send(testUser)
+  expect({
+    ...res.body.data,
+    id: '99099'
+  }).toStrictEqual(result)
+})
+
+
+
+/**
  * 退出
 */
-
 test('退出登录', async () => {
   const res = await server
     .post('/api/user/logout')
@@ -87,16 +122,44 @@ test('退出登录', async () => {
 })
 
 
+
 /**
- * 删除
- * 需再次登录，因为前面退出登录了
+ * 修改密码
 */
-test('登录成功', async () => {
+test('修改密码 登录成功', async () => {
   const res = await server
     .post('/api/user/login')
     .send({
       userName: oneUser.userName,
       password: oneUser.password
+    })
+
+  console.log('res.header', res.header)
+  cookie = res.header['set-cookie'].join(';')
+  expect(res.body.code).toBe(0)
+})
+test('修改密码', async () => {
+  const res = await server
+    .patch('/api/user/changePwd')
+    .set('cookie', cookie)
+    .send({
+      password: oneUser.password,
+      newPassword: testUser.password
+    })
+  expect(res.body.code).toBe(0)
+})
+
+
+/**
+ * 删除
+ * 需再次登录，因为前面退出登录了 使用新密码
+*/
+test('删除 登录成功', async () => {
+  const res = await server
+    .post('/api/user/login')
+    .send({
+      userName: oneUser.userName,
+      password: testUser.password
     })
 
   cookie = res.header['set-cookie'].join(';')
@@ -108,7 +171,7 @@ test('删除用户', async () => {
     .set('cookie', cookie)
     .send({
       userName: oneUser.userName,
-      password: oneUser.password
+      password: testUser.password
     })
 
   expect(res.body.code).toBe(0)
