cf08844efd160b590269958b57663a061d72a1ba
zzw <zzw1366@163.com>
2020-05-09 21:52:11 +0800

    fix bug: Sequelize 时区错误问题

--- src/db/seq.js
+++ src/db/seq.js
@@ -5,9 +5,15 @@ const { ENV } = require('../utils/env')
 const { host, database, user, passWord  } = SQL_CONF
 
 const conf = {
-  'dialect': 'mysql',  // 数据库使用mysql
+  dialect: 'mysql',  // 数据库使用mysql
   host, // 数据库服务器ip
-  'port': 3306,        // 数据库服务器端口
+  port: 3306, // 数据库服务器端口
+  pool: { // 连接池
+    max: 50,
+    min: 0,
+    idle: 10000
+  },
+  timezone: '+08:00' // 东八区
 }
 
 // 测试环境关闭日志
