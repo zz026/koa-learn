ad4d689bbfa364a32a779bb6b68ac280051d0a65
zzw <zzw1366@163.com>
2020-05-09 22:08:16 +0800

    创建外键关联

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -6,6 +6,16 @@
 const User = require('./User')
 const Blogs = require('./Blogs')
 
+// 创建外键 查询微博时查出用户
+Blogs.belongsTo(User, {
+  foreignKey: 'userId'
+})
+
+// 查询用户，查出微博
+// User.hasMany(Blogs, {
+//   foreignKey: 'userId'
+// })
+
 module.exports = {
   User,
   Blogs
